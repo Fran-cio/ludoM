5b0b34a449133005042e117c8a490fe3bd2d42ed
SanWG <sanfernandez@mi.unc.edu.ar>
2021-06-12 22:44:21 -0300

    casilleros seguros

--- LudoM.src/src/Tabla.java
+++ LudoM.src/src/Tabla.java
@@ -4,9 +4,15 @@ import java.awt.*;
 
 public class Tabla extends JFrame{
   private JPanel tablero2;
-
+    //private Panel fondo;
     private JLabel jLabel1;
-
+    private JLabel jLabel2;
+    private JLabel jLabel3;
+    private JLabel jLabel4;
+    private JLabel jLabel5;
+    private JLabel jLabel6;
+    private JLabel jLabel7;
+    private JLabel jLabel8;
     public Tabla(){
 
         this.setLayout(null);
@@ -18,7 +24,7 @@ public class Tabla extends JFrame{
         //setSize(800,800);
         this.getContentPane().setBackground(Color.darkGray);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.tablero2.setBounds(40, 40, 40, 40);
+        this.tablero2.setBounds(0, 0, 500, 500);
         this.tablero2.setBackground(new Color(190, 190, 190));
         this.tablero2.setVisible(false);
         this.add(tablero2);
@@ -29,7 +35,6 @@ public class Tabla extends JFrame{
         for (int j = 0; j < 15; j++) {
             tablero1[i][j] = new JPanel();
             tablero1[i][j].setBounds((i + 1) * 40, (j + 1) * 40, 40, 40);
-
             this.add(tablero1[i][j]);
             if ((i <= 5 && j <= 5) || (i == 1 && j == 6) || ((i > 0 && i < 7) && j == 7)) {
                 tablero1[i][j].setBackground(new Color(3, 94, 23));
@@ -47,19 +52,93 @@ public class Tabla extends JFrame{
         }
     }
 
+   /* fondo = new Panel();
+    tablero1[1][8].add(fondo);
+    tablero1[1][8].setOpaque(true);*/
 
-        jLabel1 = new JLabel();
-        jLabel1.setLayout(null);
+        jLabel1 = new JLabel(); //verde 2
+        jLabel1.setIcon(new ImageIcon("C:\\Users\\Maximiliano\\Google Drive\\Facu\\Ing de software\\TestigosDeTuring\\Imagenes\\shield.png"));
+        tablero1[2][8].setOpaque(false);
+        tablero1[2][8].add(jLabel1);
+        tablero1[2][8].repaint();
+        tablero1[2][8].revalidate();
+        this.repaint();
+        this.revalidate();
 
+        jLabel2 = new JLabel(); //rojo 2
+        jLabel2.setIcon( new ImageIcon("C:\\Users\\Maximiliano\\Google Drive\\Facu\\Ing de software\\TestigosDeTuring\\Imagenes\\shield.png"));
+        tablero1[6][2].setOpaque(false);
+        tablero1[6][2].add(jLabel2);
+        tablero1[6][2].repaint();
+        tablero1[6][2].revalidate();
+        this.repaint();
+        this.revalidate();
+
+        jLabel3 = new JLabel(); //verde salida
+        jLabel3.setIcon( new ImageIcon("C:\\Users\\Maximiliano\\Google Drive\\Facu\\Ing de software\\TestigosDeTuring\\Imagenes\\shield.png"));
+        tablero1[1][6].setOpaque(false);
+        tablero1[1][6].add(jLabel3);
+        tablero1[1][6].repaint();
+        tablero1[1][6].revalidate();
+        this.repaint();
+        this.revalidate();
+
+        jLabel4 = new JLabel(); //rojo salida
+        jLabel4.setIcon( new ImageIcon("C:\\Users\\Maximiliano\\Google Drive\\Facu\\Ing de software\\TestigosDeTuring\\Imagenes\\shield.png"));
+        tablero1[8][1].setOpaque(false);
+        tablero1[8][1].add(jLabel4);
+        tablero1[8][1].repaint();
+        tablero1[8][1].revalidate();
+        this.repaint();
+        this.revalidate();
+
+
+        jLabel5 = new JLabel();//azul salida
+        jLabel5.setIcon( new ImageIcon("C:\\Users\\Maximiliano\\Google Drive\\Facu\\Ing de software\\TestigosDeTuring\\Imagenes\\shield.png"));
+        tablero1[13][8].setOpaque(false);
+        tablero1[13][8].add(jLabel5);
+        tablero1[13][8].repaint();
+        tablero1[13][8].revalidate();
+        this.repaint();
+        this.revalidate();
+
+        jLabel6 = new JLabel(); //amarillo 2
+        jLabel6.setIcon( new ImageIcon("C:\\Users\\Maximiliano\\Google Drive\\Facu\\Ing de software\\TestigosDeTuring\\Imagenes\\shield.png"));
+        tablero1[8][12].setOpaque(false);
+        tablero1[8][12].add(jLabel6);
+        tablero1[8][12].repaint();
+        tablero1[8][12].revalidate();
+        this.repaint();
+        this.revalidate();
+
+        jLabel7 = new JLabel(); //azul 2
+        jLabel7.setIcon( new ImageIcon("C:\\Users\\Maximiliano\\Google Drive\\Facu\\Ing de software\\TestigosDeTuring\\Imagenes\\shield.png"));
+        tablero1[12][6].setOpaque(false);
+        tablero1[12][6].add(jLabel7);
+        tablero1[12][6].repaint();
+        tablero1[12][6].revalidate();
+        this.repaint();
+        this.revalidate();
+
+        jLabel8 = new JLabel(); //amarillo salida
+        jLabel8.setIcon( new ImageIcon("\\$PROJECT_DIR$\\Imagenes\\shield.png"));
+        tablero1[6][13].setOpaque(false);
+        tablero1[6][13].add(jLabel8);
+        tablero1[6][13].repaint();
+        tablero1[6][13].revalidate();
+        this.repaint();
+        this.revalidate();
+
+
+
+
+
+        /*jLabel1 = new JLabel();
         Image img= new ImageIcon("C:\\Users\\Maximiliano\\Google Drive\\Facu\\Ing de software\\TestigosDeTuring\\Imagenes\\shield.png").getImage();
-        //ImageIcon img2=new ImageIcon(img.getScaledInstance(tablero1[1][8].getWidth(),tablero1[1][8].getHeight(), Image.SCALE_SMOOTH));
-        ImageIcon img2=new ImageIcon(img.getScaledInstance(tablero1[1][9].getWidth(),tablero1[1][9].getHeight(), Image.SCALE_SMOOTH));
-
+        ImageIcon img2=new ImageIcon(img.getScaledInstance(tablero1[1][8].getWidth(), tablero1[1][8].getHeight(), Image.SCALE_SMOOTH));
         jLabel1.setIcon(img2);
+        tablero1[1][8].add(jLabel1);*/
 
-        tablero1[1][9].add(jLabel1);
-        //tablero2.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        //tablero2.add(jLabel1);*/
 
 
 
